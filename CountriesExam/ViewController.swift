30491c441d0e371fa364e19d8da0189ea238412e
deinerjohn <djcalbang@gmail.com>
2021-03-18 08:52:35 +0800

    March 18 2021
    Start of the exam

---
+++ CountriesExam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CountriesExam
+//
+//  Created by Blue Solution Networks on 3/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+    }
+
+}
+
